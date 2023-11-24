b7d5f3960a9933bfd2d0c1e6324c17238113548d
Kayke-Ti <kaykeloiola@gmail.com>
2023-11-24 10:47:02 -0300

    raw: configuring icons

--- src/components/ProfilePage/styles.ts
+++ src/components/ProfilePage/styles.ts
@@ -2,22 +2,29 @@ import styled, { css } from "styled-components";
 
 import { Cake, LocationOn } from "../../styles/Icons";
 
-export const Avatar = styled.div``;
+export const Container = styled.div`
+
+`;
+
 export const Banner = styled.div``;
-export const CakeIcon = styled.div``;  
-export const Container = styled.div``;
+export const Avatar = styled.div``;
+export const ProfileData = styled.div``;
 
 const iconCSS = css`
   width: 20px;
   height: 20px;
-
+  
   color: var(--gray);
-`;
+  `;
 
 
-export const Followage = styled(LocationOn)`${iconCSS}`;
-export const LocationIcon = styled(Cake)`${iconCSS}`;
+export const LocationIcon = styled(LocationOn)`
+  ${iconCSS}`
+;
+export const CakeIcon = styled(Cake)`
+  ${iconCSS}`
+;
+  
+export const Followage = styled.div``;
 
-export const ProfileData = styled.div`
 
-`;
